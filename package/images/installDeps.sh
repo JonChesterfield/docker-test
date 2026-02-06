b54bd10e155fe5f443ebdc6ba7b2c12b14c3d4ec
Jon Chesterfield <jon@spectralcompute.co.uk>
2026-02-06 20:35:45 +0000

    older dockers can't do heredoc, workaround

---
+++ package/images/installDeps.sh
@@ -0,0 +1,112 @@
+#!/bin/bash
+
+set -ETeuo pipefail
+
+apt-get update
+
+# Install various packages we need for testing.
+    PACKAGES=(
+        # CI system.
+        rsync
+
+        # Packages for testing.
+        libbenchmark-dev
+        libgmock-dev
+        libgtest-dev
+
+        # Various projects.
+        imagemagick
+        python3
+
+        # Alien.
+        curl
+        libgl-dev
+        libglu1-mesa-dev
+        libxcursor-dev
+        libxi-dev
+        libxinerama-dev
+        xorg-dev
+        zip
+
+        # CUTLASS
+        bc
+
+        # Cycles
+        git-lfs
+        libavcodec-dev
+        libavdevice-dev
+        libavfilter-dev
+        libavformat-dev
+        libavutil-dev
+        libboost-dev
+        libembree-dev
+        libgflags-dev
+        libgoogle-glog-dev
+        libmagickcore-dev
+        libopenimageio-dev
+        libopenjp2-7-dev
+        libosd-dev
+        libpng-dev
+        libpostproc-dev
+        libpugixml-dev
+        libswresample-dev
+        libswscale-dev
+        libtiff-dev
+        libwebp-dev
+        time
+        zlib1g-dev
+
+        # GPUJPEG
+        ffmpeg
+
+        # xgboost
+        python3-build
+        python3-hatchling
+        python3-wheel
+        python3-pandas
+        python3-scipy
+        python3-sklearn
+
+        # Faiss
+        libopenblas-dev
+        python3-numpy
+        python3-installer
+        swig
+
+        # TCLB
+        r-base
+        r-base-dev
+        libopenmpi-dev
+
+        # FastEddy
+        libnetcdf-dev
+
+        # pytorch
+        python3-setuptools
+        python3-yaml
+
+        # FastEddy
+        jupyter-notebook
+        python3-xarray
+
+        # caffe
+        libboost-filesystem-dev
+        libboost-system-dev
+        libboost-thread-dev
+        libhdf5-serial-dev
+        libleveldb-dev
+        liblmdb-dev
+        libopencv-dev
+        libprotobuf-dev
+        libsnappy-dev
+        protobuf-compiler
+
+        # hashinator
+        meson
+
+        # arrayfire
+        libfftw3-dev
+        libatlas-base-dev
+    )
+    
+    apt-get install -y "${PACKAGES[@]}"
